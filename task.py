386c91e30af764bda56f137893d582467b54924b
Israel Polasak <146323213+SrulyP@users.noreply.github.com>
2025-03-06 17:53:09 +0200

    Fill out starter calculations for my_datetime function

--- task.py
+++ task.py
@@ -3,7 +3,18 @@ def conv_num(num_str):
 
 
 def my_datetime(num_sec):
-    pass
+    month = 1
+    day = 1
+    year = 1970
+    minutes = num_sec/60
+    hours = minutes/60
+    days = hours/24
+    # calculate years and leap years
+    print(f'{month}-{day}-{year}')
+
+
+def my_datetime_helper_function(year):
+    return (year % 4 == 0 and year % 100 != 0) or (year % 400 == 0)
 
 
 def conv_endian(num, endian='big'):
